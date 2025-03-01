25c65ef0a7269b59cc7ae2ecd19a411aab460839
Jatin Talgotra <jatintalgotra2004@gmail.com>
2025-03-02 00:22:51 +0530

    added MySQL connectivity

--- banking-management-system/src/main/java/com/jatin/bankingsystem/models/Customer.java
+++ banking-management-system/src/main/java/com/jatin/bankingsystem/models/Customer.java
@@ -3,20 +3,19 @@ package com.jatin.bankingsystem.models;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "CustomerTable")
 public class Customer extends User{
     @OneToOne(
         cascade = CascadeType.ALL,
-        fetch = FetchType.LAZY,
-        mappedBy = "CustomerTable"
+        fetch = FetchType.LAZY
     )
+    @JoinColumn(name = "acc_id")
     private Account account;
     // private String accountNumber;
 
